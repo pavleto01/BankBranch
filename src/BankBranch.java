2f13c34306c9cda4339befe82e21a56ffca6f7a9
Pavel Todorov <pavleto01@gmail.com>
2023-01-16 15:54:15 +0200

    Last commit

--- src/BankBranch.java
+++ src/BankBranch.java
@@ -1,10 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BankBranch {
+
     private static BankBranch instance;
-    private BankAccount[] accounts;
-    private int accountCount;
+    private Map<Integer, BankAccount> accounts;
 
     private BankBranch() {
-        accounts = new BankAccount[10];
+        accounts = new HashMap<>();
     }
 
     public static BankBranch getInstance() {
@@ -15,86 +18,71 @@ public class BankBranch {
     }
 
     public boolean isaccountNumberUnique(int accountNumber) {
-        for (int i = 0; i < accountCount; i++) {
-            if (accounts[i].getAccountNumber() == accountNumber) {
-                return false;
-            }
-        }
-        return true;
+        return !accounts.containsKey(accountNumber);
     }
 
-    public void createAccount(int accountNumber,String customerName, double balance, boolean isSpecial) {
-        if(accountNumber <= 0){
-            System.out.println("Invalid account number");
-            return;
-        }
-        if(customerName == null || customerName.trim().length() == 0){
-            System.out.println("Invalid customer name");
-            return;
-        }
-        if(balance < 0){
-            System.out.println("Invalid balance");
-            return;
-        }
+    public void createAccount(int accountNumber, String customerName, double balance, boolean isSpecial) {
         BankAccount account;
         if (isSpecial) {
-            account = new SpecialBankAccount(accountNumber,customerName, balance);
+            account = new SpecialBankAccount(accountNumber, customerName, balance);
         } else {
-            account = new NormalBankAccount(accountNumber,customerName, balance);
+            account = new NormalBankAccount(accountNumber, customerName, balance);
         }
-        accounts[accountCount++] = account;
+        accounts.put(accountNumber, account);
     }
 
-    public int searchAccount(int accountNumber) {
-        for (int i = 0; i < accountCount; i++) {
-            if (accounts[i] != null && accounts[i].getAccountNumber()== accountNumber) {
-                return i;
-            }
+    public void removeAccount(int accountNumber) {
+        BankAccount account = accounts.get(accountNumber);
+        if (account == null) {
+            System.out.println("Account not found.");
+            return;
         }
-        return -1;
+        account.withdrawAll(account.getBalance());
+        accounts.remove(accountNumber);
     }
 
-    public void removeAccount(int accountIndex) {
-        accounts[accountIndex].withdrawAll(accounts[accountIndex].getBalance());
-        accounts[accountIndex] = null;
+    public void deposit(int accountNumber, double amount) {
+        BankAccount account = accounts.get(accountNumber);
+        if (account == null) {
+            System.out.println("Account not found.");
+            return;
+        }
+        account.deposit(amount);
+    }
+    public void withdraw(int accountNumber, double amount) {
+        BankAccount account = accounts.get(accountNumber);
+        if (account == null) {
+            System.out.println("Account not found.");
+            return;
+        }
+        account.withdraw(amount);
     }
 
-    public void deposit(int accountIndex, double amount) {
-        if (accounts[accountIndex] != null) {
-            accounts[accountIndex].deposit(amount);
+    /*public double checkBalance(int accountNumber) {
+        BankAccount account = accounts.get(accountNumber);
+        if (account == null) {
+            System.out.println("Account not found.");
+            return -1;
+        }
+        return account.getBalance();
+    }*/
+
+    public void checkBalance(int accountNumber) {
+        BankAccount account = accounts.get(accountNumber);
+        if (account != null) {
+            System.out.println("Account number: "+account.getAccountNumber());
+            System.out.println("Customer Name: "+account.getCustomerName());
+            System.out.println("Balance: " + account.getBalance());
         } else {
             System.out.println("Invalid account.");
         }
     }
-    public void withdraw(int accountIndex, double amount) {
-        if (accounts[accountIndex] != null) {
-            accounts[accountIndex].withdraw(amount);
-        } else {
-            System.out.println("Invalid account.");
-        }
-    }
 
-    public void checkBalance(int accountIndex) {
-        if (accounts[accountIndex] != null) {
-            System.out.println("Customer Name: "+accounts[accountIndex].getCustomerName());
-            System.out.println("Balance: " + accounts[accountIndex].getBalance());
-        } else {
-            System.out.println("Invalid account.");
-        }
-    }
-
-    private String typeOfAccount(boolean type) {
-        return type ? "Special" : "Normal";
-    }
-
-    public void printAccount() {
-        for (int i = 0; i < accountCount; i++) {
-            if (accounts[i] != null) {
-                System.out.println("Account type: " + typeOfAccount(accounts[i] instanceof SpecialBankAccount));
-                System.out.println("Customer Name: " + accounts[i].getCustomerName());
-                System.out.println("Balance: " + accounts[i].getBalance());
-                System.out.println("-------------------");
-            }
+    public void printAllAccounts() {
+        for (Map.Entry<Integer, BankAccount> entry : accounts.entrySet()) {
+            System.out.println("Account Number: " + entry.getKey());
+            System.out.println("Customer Name: " + entry.getValue().getCustomerName());
+            System.out.println("Balance: " + entry.getValue().getBalance());
         }
     }
 }
--- src/ConsoleMenu.java
+++ src/ConsoleMenu.java
@@ -1,102 +1,132 @@
 import java.util.Scanner;
 
 public class ConsoleMenu {
-    private int menu(Scanner sc){
+    private static final Scanner scanner = new Scanner(System.in);
+    private static BankBranch bankBranch = BankBranch.getInstance();
+
+    public static void run() {
+        while (true) {
+            printMenu();
+            int choice = getChoice();
+            performAction(choice);
+        }
+    }
+
+    private static void printMenu() {
         System.out.println("1. Create Account");
         System.out.println("2. Remove Account");
         System.out.println("3. Deposit");
         System.out.println("4. Withdraw");
         System.out.println("5. Check Balance");
-        System.out.println("6. Print all accounts");
+        System.out.println("6. Print All Accounts");
         System.out.println("7. Exit");
         System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
+    }
+
+    private static int getChoice() {
+        int choice = scanner.nextInt();
+        scanner.nextLine();
         return choice;
     }
 
-    public static void executeCommands() {
-        BankBranch bank = BankBranch.getInstance();
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int choice = new ConsoleMenu().menu(sc);
 
-            switch (choice) {
-                case 1:
-                    int accountNumber;
-                    do {
-                        System.out.print("Enter account number: ");
-                        accountNumber = sc.nextInt();
-                        if (!bank.isaccountNumberUnique(accountNumber)) {
-                            System.out.println("Account number already exists.");
-                        }
-                    } while (bank.isaccountNumberUnique(accountNumber) == false);
-                    System.out.print("Enter customer name: ");
-                    String customerName = sc.next();
-                    System.out.print("Enter initial deposit: ");
-                    double deposit = sc.nextDouble();
-                    int type;
-                    do {
-                        System.out.print("1: Normal Account, 2: Special Account: \n");
-                        System.out.print("Type 1 or 2 for account type: ");
-                        type = sc.nextInt();
-                    } while(type < 1 || type > 2);
-                    boolean isSpecial = type == 2;
-                    bank.createAccount(accountNumber,customerName, deposit, isSpecial);
-                    break;
-                case 2:
-                    System.out.print("Enter account number: ");
-                    accountNumber = sc.nextInt();
-                    int accountIndex = bank.searchAccount(accountNumber);
-                    if (accountIndex != -1) {
-                        bank.removeAccount(accountIndex);
-                        System.out.println("Account removed successfully.");
-                    } else {
-                        System.out.println("Account not found.");
-                    }
-                    break;
-                case 3:
-                    System.out.print("Enter account number: ");
-                    accountNumber = sc.nextInt();
-                    accountIndex = bank.searchAccount(accountNumber);
-                    if (accountIndex != -1) {
-                        System.out.print("Enter deposit amount: ");
-                        deposit = sc.nextDouble();
-                        bank.deposit(accountIndex, deposit);
-                    } else {
-                        System.out.println("Account not found.");
-                    }
-                    break;
-                case 4:
-                    System.out.print("Enter account number: ");
-                    accountNumber = sc.nextInt();
-                    accountIndex = bank.searchAccount(accountNumber);
-                    if (accountIndex != -1) {
-                        System.out.print("Enter withdraw amount: ");
-                        double withdraw = sc.nextDouble();
-                        bank.withdraw(accountIndex, withdraw);
-                    } else {
-                        System.out.println("Account not found.");
-                    }
-                    break;
-                case 5:
-                    System.out.print("Enter account number: ");
-                    accountNumber = sc.nextInt();
-                    accountIndex = bank.searchAccount(accountNumber);
-                    if (accountIndex != -1) {
-                        bank.checkBalance(accountIndex);
-                    } else {
-                        System.out.println("Account not found.");
-                    }
-                    break;
-                case 6:
-                    bank.printAccount();
-                    break;
-                case 7:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
+    private static void createAccount() {
+        System.out.println("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        if (!bankBranch.isaccountNumberUnique(accountNumber)) {
+            System.out.println("Account number already exists. Please try again.");
+            return;
+        }
+        System.out.println("Enter customer name: ");
+        String customerName = scanner.nextLine();
+        System.out.println("Enter initial balance: ");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+        int type;
+        do {
+            System.out.print("1: Normal Account, 2: Special Account: \n");
+            System.out.print("Type 1 or 2 for account type: ");
+            type = scanner.nextInt();
+        } while(type < 1 || type > 2);
+        boolean isSpecial = type == 2;
+        bankBranch.createAccount(accountNumber, customerName, balance, isSpecial);
+        System.out.println("Account created successfully!");
+    }
+
+    private static void removeAccount() {
+        System.out.println("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        bankBranch.removeAccount(accountNumber);
+        System.out.println("Account removed successfully!");
+    }
+
+    private static void deposit() {
+        System.out.println("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter amount to deposit: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        bankBranch.deposit(accountNumber, amount);
+        System.out.println("Deposit successful!");
+    }
+
+    private static void withdraw() {
+        System.out.println("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        bankBranch.withdraw(accountNumber, amount);
+    }
+
+    private static void checkBalance() {
+        System.out.println("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        bankBranch.checkBalance(accountNumber);
+    }
+
+    private static void printAllAccounts() {
+        bankBranch.printAllAccounts();
+    }
+
+    private static void exit() {
+        System.out.println("Thank you for using our bank!");
+        System.exit(0);
+    }
+
+    private static void performAction(int choice) {
+        switch (choice) {
+            case 1:
+                createAccount();
+                break;
+            case 2:
+                removeAccount();
+                break;
+            case 3:
+                deposit();
+                break;
+            case 4:
+                withdraw();
+                break;
+            case 5:
+                checkBalance();
+                break;
+            case 6:
+                printAllAccounts();
+                break;
+            case 7:
+                exit();
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
         }
     }
+
+
 }
